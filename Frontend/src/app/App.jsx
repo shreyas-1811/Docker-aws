68d1e582f85a65130d93f0fcca7f944d480e29be
shreyas-1811 <shreyaschandrashekar51@gmial.com>
2026-06-25 15:37:29 +0530

    basic ui setup

--- Frontend/src/app/App.jsx
+++ Frontend/src/app/App.jsx
@@ -1,34 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css"
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className = "h-screen w-full bg-gray-950 flex gap-4 p-4">
+      <aside className = "h-full w-1/4 bg-amber-50 rounded-lg"
+      ></aside>
+      <section
+        className = "w-3/4 bg-neutral-800 rounded-lg">
+      </section>
+    </main>
   )
 }
 

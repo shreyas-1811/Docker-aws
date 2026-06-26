446438f4fdfbd5faf7fc437c51e40a8a3b621ceb
shreyas-1811 <shreyaschandrashekar51@gmial.com>
2026-06-26 15:45:45 +0530

    docker file added

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import { Server } from "socket.io"
 import {YSocketIO} from "y-socket.io/dist/server"
 
 const app = express()
+app.use(express.static("public"))  //This will serve the static files from the public folder
 const httpServer = createServer(app)
 
 const io = new Server(httpServer, {
@@ -17,13 +18,6 @@ const ySocketIO = new YSocketIO(io)
 ySocketIO.initialize()
 
 
-
-app.get("/", (req, res) => {
-    res.status(200).json({
-        message: "Hello World",
-        success: true
-    })
-})
 
 app.get('/health',(req,res) => {
     res.status(200).json({
